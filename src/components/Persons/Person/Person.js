594d3890cc8f741b25a474475398ce9206104f56
Alessandro De Cristofaris <alessandrodecristofaris@AirdiAlessandro.homenet.telecomitalia.it>
2020-02-01 14:35:19 +0100

    Fino a lezione 108

---
+++ src/components/Persons/Person/Person.js
@@ -0,0 +1,20 @@
+import React, { Component, Fragment } from 'react'; //Fragment è la stessa cosa di Aux, serve per inserire nel JSX più elementi adiacenti
+import Aux from '../../../hoc/Aux';
+import withClass from '../../../hoc/withClass';
+import classes from './Person.css';
+
+class Person extends Component {
+    render(){
+        console.log("[Person.js] rendering");
+        return (
+            <Aux>
+                <p key="i1" onClick={this.props.click}>I'm a {this.props.name} and I am {this.props.age} years old!</p>,
+                <p key="i2">{this.props.children}</p>,
+                <input key="i3" type="text" onChange= {this.props.changed} value={this.props.name} />
+            </Aux>
+        )
+    }
+    
+}
+
+export default withClass(Person, classes.Person);
---
+++ src/containers/App.js
@@ -0,0 +1,110 @@
+import React, { Component } from 'react';
+import classes from './App.css';
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
+import withClass from '../hoc/withClass';
+import Aux from '../hoc/Aux';
+
+class App extends Component {
+
+  constructor(props){ //1
+    super(props);
+    console.log("[App.js] constructor");
+  }
+
+  state = {
+    persons: [
+      { id: 'asfa1', name: 'Max', age: 28 },
+      { id: 'vasd1', name: 'Manu', age: 29 },
+      { id: 'asdf11', name: 'Stephanie', age: 26 }
+    ],
+    otherState: 'some other value',
+    showPersons: false,
+    showCockpit: true,
+    changeCounter: 0
+  };
+
+  static getDerivedStateFromProps(props, state){ //2
+    console.log("[App.js] getDerivedStatePros", props);
+    return state;
+  }
+
+  componentDidMount(){
+    console.log("[App.js] componentDidMount");
+  }
+
+  shouldComponentUpdate(nextProps, nextState){
+    console.log("[App.js] shouldComponentUpdate");
+    return true;
+  }
+
+  componentDidUpdate(){
+    console.log("[App.js] componentDidUpdate");
+  }
+
+  deletePersonHandler = (personIndex) => {
+    //const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons});
+  }
+
+  changeNameHandler = (event, id) => {
+
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    })
+
+    const person = {...this.state.persons[personIndex]};
+    person.name = event.target.value;
+    //ulteriore approccio per creare un nuovo obj const person = Object.assign({}, this.state.persons[personIndex]);
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState((prevState, props) => {
+      return {
+        persons: persons, 
+        changeCounter: prevState.changeCounter +1      
+      };
+    });
+  };
+
+  togglePersonsHandler = () =>{
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow});
+  }
+
+  render() { //3
+    console.log("[App.js] render");
+    let persons = null;
+
+    if(this.state.showPersons){
+      persons = 
+          <Persons 
+            persons={this.state.persons}
+            clicked={this.deletePersonHandler}
+            changed={this.changeNameHandler}
+          />
+    }
+
+    return (
+        <Aux classes={classes.App}>
+          <button onClick={() =>{ this.setState({showCockpit:false})}}> 
+            Remove Cockpit
+          </button>
+          {this.state.showCockpit ? (
+            <Cockpit 
+              title={this.props.appTitle}
+              showPersons={this.state.showPersons}
+              personsLength={this.state.persons.length}
+              clicked={this.togglePersonsHandler}
+          />) : null}
+          {persons}
+        </Aux>
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+  }
+}
+
+export default withClass(App, classes.App);
